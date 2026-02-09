1d7cabb759af40a2a4086eb586b94d5a8d80b8c6
abdcantcode <abdullah72ftw@gmail.com>
2026-02-09 17:17:35 +0530

    chore: 3rd problem

--- src/03-grade-calculator.js
+++ src/03-grade-calculator.js
@@ -25,5 +25,33 @@
  * @returns {string} The letter grade or "INVALID"
  */
 export function calculateGrade(score, hasExtraCredit) {
-  // Your code here
+  if (score < 0 || score > 100) {
+    return "INVALID";
+  } 
+  if(hasExtraCredit){
+    if(score>=95){
+      score=100;
+    }
+    else{
+      score=score+5;
+    }
+    
+  }
+  if(score<0||score>100){
+    return "INVALID";
+  }
+  if(score>=0 && score<=59){
+    return "F";
+  }else if(score>=60 && score<=69){
+    return "D";
+  }
+  else if(score>=70 && score<=79){
+    return "C";
+  }
+  else if(score>=80 && score<=89){
+    return "B";
+  }
+  else if(score>=90 && score<=100){
+    return "A";
+  }
 }
