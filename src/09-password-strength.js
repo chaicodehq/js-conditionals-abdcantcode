6e33fe5be5c6c0d4c7b780245fdaac972db692af
abdcantcode <abdullah72ftw@gmail.com>
2026-02-09 20:15:31 +0530

    chore: 9th done

--- src/09-password-strength.js
+++ src/09-password-strength.js
@@ -26,5 +26,33 @@
  * @returns {string} "weak", "medium", "strong", or "very strong"
  */
 export function checkPasswordStrength(password) {
-  // Your code here
+  if(typeof password!=="string" || password.length===0){
+    return "weak"
+  }
+  let strength=0;
+  if(password.length>=8){
+    strength+=1;
+  }
+  if((/[a-z]/).test(password)){
+    strength+=1;
+  }
+  if((/[A-Z]/).test(password)){
+    strength+=1;
+  }
+  if((/[0-9]/).test(password)){
+    strength+=1;
+  }
+  if(/[!@#$%^&*()_+\-=\[\]{}|;:,.<>?]/.test(password)){
+    strength+=1;
+  }
+  if(strength<=1){
+    return "weak";
+  }else if(strength>=2 && strength<=3){
+    return "medium";
+  }else if(strength===4){
+    return "strong";
+  }
+  else{
+    return "very strong";
+  }
 }
